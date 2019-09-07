9062dc699ca56d5105f2178aef9dc73a7b1f3576
Joe Lee <josephalanlee@gmail.com>
2019-09-07 22:07:18 +0100

    Added ThoughtController & service

---
+++ Application/Entropy/entropy-api/entropy-api/entropy-api/Controllers/v0/Thoughts/IThoughtService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using entropy.entities;
+
+namespace entropyapi.Controllers.v0.Thoughts
+{
+    public interface IThoughtRepository
+    {
+        void InsertThought(Thought thought);
+    }
+}
---
+++ Application/Entropy/entropy-api/entropy-api/entropy-api/Controllers/v0/Thoughts/ThoughtService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using entropy.entities;
+
+namespace entropyapi.Controllers.v0.Thoughts
+{
+    public class ThoughtRepository: IThoughtRepository, IDisposable
+    {
+
+        private EntropyContext context;
+
+        public ThoughtRepository(EntropyContext context)
+        {
+            this.context = context;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertThought(Thought thought)
+        {
+            this.context.Thoughts.Add(thought);
+        }
+
+        
+    }
+}
