f81d73d82768d6e92b87feeb065349d1afdaef01
Joe Lee <josephalanlee@gmail.com>
2019-10-22 21:03:45 +0100

    Added In Memory Context Provider

--- Application/Entropy/entropy-api/entropy-api/entropy-api.tests/helpers/InMemoryContextProvider.cs
+++ Application/Entropy/entropy-api/entropy-api/entropy-api.tests/helpers/InMemoryContextProvider.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using entropy.entities;
+using Microsoft.EntityFrameworkCore;
+
 namespace entropy_api.tests.helpers
 {
-    public class InMemoryContextProvider
+    public class InMemoryOptionsHelper
     {
-        public InMemoryContextProvider()
+        public InMemoryOptionsHelper()
         {
+        }
+
+        public static DbContextOptions<EntropyContext> BuildNewOptions()
+        {
+            return new DbContextOptionsBuilder<EntropyContext>()
+                .UseInMemoryDatabase(databaseName: "Entropy").Options;
         }
     }
 }
