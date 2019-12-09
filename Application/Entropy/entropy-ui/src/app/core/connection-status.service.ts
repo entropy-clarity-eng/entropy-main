dcff154f45d8026974f95fc2d80df521a4c2ec6b
Joe Lee <josephalanlee@gmail.com>
2019-12-09 22:25:56 +0000

    Added Summary to Connection status service

--- Application/Entropy/entropy-ui/src/app/core/connection-status.service.ts
+++ Application/Entropy/entropy-ui/src/app/core/connection-status.service.ts
@@ -12,6 +12,12 @@ export class ConnectionStatusService {
   private onlineEvent = fromEvent(window,"online");
   private initialState:Observable<boolean>;
   
+
+  /* 
+     This Service can be used to detect the presence of a network connection, but it doesn't prove internet connectivity.
+     That can only be done by sending a request and seeing whether it was successful. 
+  */
+  
   constructor(readonly windowProxy:any, readonly navigatorProxy:any) {
 
    
