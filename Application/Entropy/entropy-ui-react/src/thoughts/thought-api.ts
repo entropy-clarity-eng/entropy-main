571e63b99f6d4864aac360d1af82b1b9c1b07c63
Joe Lee <josephalanlee@gmail.com>
2020-04-24 21:59:58 +0100

    Got a thought to persist!

--- Application/Entropy/entropy-ui-react/src/thoughts/thought-api.ts
+++ Application/Entropy/entropy-ui-react/src/thoughts/thought-api.ts
@@ -4,8 +4,13 @@ import { APIPersistenceError } from '../general/api-persistence-error';
 
 export class ThoughtAPI {
 
+    
+
     static async addThought(thought:ThoughtModel):Promise<null | APIPersistenceError> {
-        return axios.post(`${process.env.REACT_APP_BASE_API_URL}/thoughts`,thought)
+        return axios.post(`${process.env.REACT_APP_BASE_API_URL}/thoughts`,thought,{
+            headers:[{'content-type': 'application/json'}]
+        
+        })
                .then(result => null,error => new APIPersistenceError(error));
     }
 
