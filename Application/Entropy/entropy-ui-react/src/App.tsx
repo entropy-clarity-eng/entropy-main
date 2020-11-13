7dbddd99be60d7ba63f93fffdb8b4b2f33ff9bcb
Joe Lee <josephalanlee@gmail.com>
2020-11-13 08:41:26 +0000

    Plugged icons into thought persistence

--- Application/Entropy/entropy-ui-react/src/App.tsx
+++ Application/Entropy/entropy-ui-react/src/App.tsx
@@ -3,17 +3,21 @@ import './App.css';
 import CollectThought from './thoughts/collect-thought';
 import { AppContainer } from './app-container';
 import { AppTopBar } from './app-top-bar';
+import { useRootStore } from '.';
 
 
 function App() {
+  
+  const {thoughtStore} = useRootStore();
+
   return (
     <AppContainer>
           <AppTopBar>
        <div className="menu-container">Menu</div>
       <div className="title">Entropy</div>
       <div className="upload-status-container">
-        <img className="cloud-icon" src="/icons/upload-complete.png" alt="ok"/>
-        <img className="cloud-icon" src="/icons/upload-in-progress.png" alt="ok"/>
+       {thoughtStore.currentPersistenceStatus.thoughtsLeftToPersist === 0 && <img className="cloud-icon" src="/icons/upload-complete.png" alt="ok"/>} 
+       {thoughtStore.currentPersistenceStatus.thoughtsLeftToPersist > 0 &&  <img className="cloud-icon" src="/icons/upload-in-progress.png" alt="ok"/>}
         </div>
        </AppTopBar>
         <CollectThought />
